9fef9b565625bee0d067a58201143071b4bcce70
Zou,Yong <zoumengdie@gmail.com>
2014-08-28 18:18:00 +0800

    Change app icon and some UI

--- src/com/chriszou/apptimer/app/TimerReceiver.java
+++ src/com/chriszou/apptimer/app/TimerReceiver.java
@@ -11,7 +11,6 @@ import android.content.Intent;
 
 import com.chriszou.apptimer.alarm.AlarmExecutor;
 import com.chriszou.apptimer.alarm.AlarmHelper;
-import com.chriszou.apptimer.appmanager.AppManager;
 import com.chriszou.apptimer.model.AlarmItem;
 
 /**
